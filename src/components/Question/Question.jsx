dc4ace64072d62c4152326c0f7149552406c8798
rajat <rajat2411gupta@gmail.com>
2022-02-01 15:11:38 +0530

    rendering ques

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -2,36 +2,45 @@ import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 
 const Question = (props) => {
-  const [options, setOptions] = useState([]);
 
-  useEffect(() => {
-    getOptions();
-  }, []);
+  console.log(props)
 
-  const getOptions = () => {
-    let arr = [];
-    props.question.answers.answer_a !== null && arr.push(props.question.answers.answer_a);
-    props.question.answers.answer_b !== null && arr.push(props.question.answers.answer_b);
-    props.question.answers.answer_c !== null && arr.push(props.question.answers.answer_c);
-    props.question.answers.answer_d !== null && arr.push(props.question.answers.answer_d);
-    props.question.answers.answer_e !== null && arr.push(props.question.answers.answer_e);
-    props.question.answers.answer_f !== null && arr.push(props.question.answers.answer_f);
-    setOptions(arr);
-  };
+  // const [options, setOptions] = useState([]);
+
+  // useEffect(() => {
+  //   getOptions();
+  // }, []);
+
+  // const getOptions = () => {
+  //   let arr = [];
+  //   props.question.answers.answer_a !== null && arr.push(props.question.answers.answer_a);
+  //   props.question.answers.answer_b !== null && arr.push(props.question.answers.answer_b);
+  //   props.question.answers.answer_c !== null && arr.push(props.question.answers.answer_c);
+  //   props.question.answers.answer_d !== null && arr.push(props.question.answers.answer_d);
+  //   props.question.answers.answer_e !== null && arr.push(props.question.answers.answer_e);
+  //   props.question.answers.answer_f !== null && arr.push(props.question.answers.answer_f);
+
+  //   props.question.correct_answer === "answer_a" && props.setCorrectAnswer(0);
+  //   props.question.correct_answer === "answer_b" && props.setCorrectAnswer(1);
+  //   props.question.correct_answer === "answer_c" && props.setCorrectAnswer(2);
+  //   props.question.correct_answer === "answer_d" && props.setCorrectAnswer(3);
+  //   props.question.correct_answer === "answer_e" && props.setCorrectAnswer(4);
+  //   props.question.correct_answer === "answer_f" && props.setCorrectAnswer(5);
+
+  //   setOptions(arr);
+  // };
 
   return (
     <div className={styles.questionBox}>
-      <p className={styles.question}>{props.question.question}</p>
+      {/* <p className={styles.question}>{props.question.question}</p> */}
       <ol className={styles.options}>
-        {
+        {/* {
           options.length !== 0 && (
-            options.map((option, i) => {
-              return (
-                <li className={styles.option} key={i}>{option}</li>
-              )
-            })
+            options.map((option, i) => 
+              <li onClick={() => props.setAnswer(i)} className={props.answer === i ? styles.answer : styles.option} key={i}>{option}</li>
+            )
           )
-        }
+        } */}
       </ol>
     </div>
   );
--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,32 +1,97 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import Question from '../../components/Question/Question';
-
+// import Question from '../../components/Question/Question';
 import styles from "./styles.module.css";
 
 const QuizPage = () => {
+
   const [question, setQuestion] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [answer, setAnswer] = useState(null);
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+  const [options, setOptions] = useState([]);
 
   useEffect(() => {
     getQuestion();
   }, []);
 
+  useEffect(() => {
+    getOptions();
+  }, [question, currentIndex])
+
   const getQuestion = async () => {
     try {
-      const res = await axios.get(`https://quizapi.io/api/v1/questions?apiKey=${process.env.REACT_APP_API_KEY}&category=uncategorized&difficulty=Easy&limit=10`);
-      setQuestion(res.data);  
+      const res = await axios.get(`https://quizapi.io/api/v1/questions?apiKey=${process.env.REACT_APP_API_KEY}&category=Linux&difficulty=Easy&limit=10`);
+      setQuestion(res.data);
     } catch (error) {
       console.log(error);
     }
   }
 
+  const getOptions = () => {
+    let arr = [];
+    if (question.length > 0){
+      question[currentIndex].answers.answer_a !== null && arr.push(question[currentIndex].answers.answer_a);
+      question[currentIndex].answers.answer_b !== null && arr.push(question[currentIndex].answers.answer_b);
+      question[currentIndex].answers.answer_c !== null && arr.push(question[currentIndex].answers.answer_c);
+      question[currentIndex].answers.answer_d !== null && arr.push(question[currentIndex].answers.answer_d);
+      question[currentIndex].answers.answer_e !== null && arr.push(question[currentIndex].answers.answer_e);
+      question[currentIndex].answers.answer_f !== null && arr.push(question[currentIndex].answers.answer_f);
+  
+      question[currentIndex].correct_answer === "answer_a" && setCorrectAnswer(0);
+      question[currentIndex].correct_answer === "answer_b" && setCorrectAnswer(1);
+      question[currentIndex].correct_answer === "answer_c" && setCorrectAnswer(2);
+      question[currentIndex].correct_answer === "answer_d" && setCorrectAnswer(3);
+      question[currentIndex].correct_answer === "answer_e" && setCorrectAnswer(4);
+      question[currentIndex].correct_answer === "answer_f" && setCorrectAnswer(5);
+      
+      setOptions(arr);
+    }
+  };
+
+  const checkAnswer = () => {
+    console.log("correct", correctAnswer)
+    console.log("select", answer)
+
+    if (correctAnswer === answer) console.log("right +++++++");
+    else console.log("wrong -------")
+
+    // if (correctAnswer === null || answer === null) return;
+
+    // else if (correctAnswer === answer) {
+    //   console.log("correct");
+      
+    //   getOptions();
+
+    // }
+
+    if(currentIndex <= 9) {
+      console.log(currentIndex)
+      setCurrentIndex(currentIndex+1);
+    }
+    else {
+      console.log("game OVERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR");
+    }
+  }
+
   return (
     <div className={styles.box}>
+      {currentIndex}
+
       {
-        question.length !== 0 && <Question question={question[0]} />
+        question.length !== 0 && <p>{question[currentIndex].question}</p>
       }
+      
+      {
+        options.length !== 0 && (
+          options.map((option, i) => 
+            <li onClick={() => setAnswer(i)} className={styles.option} key={i}>{option}</li>
+          )
+        )
+      }
+
       <div className={styles.btns}>
-        <button className={styles.checkBtn}>Check</button>
+        <button onClick={checkAnswer} className={styles.checkBtn}>Check</button>
       </div>
     </div>
   );
